48e8748993cbfc894a574f4f10e3f80a4725243b
JelleLagerweij <v.j.lagerweij@student.tudelft.nl>
2023-02-12 14:06:53 +0100

    restructured ready to run more

--- RUN.sh
+++ RUN.sh
@@ -1,41 +1,48 @@
 #!/bin/bash
 
-for Temp in 298.15
+for Nrun in 5000000 10000000 15000000
 do
-	mkdir temp_${Temp%.*}
-	cd temp_${Temp%.*}
+	mkdir time_$Nrun
+	cd time_$Nrun
 
-	for m in 3
+	for Temp in 298.15
 	do
-		mkdir molality_$m
-		cd molality_$m
+		mkdir temp_${Temp%.*}
+		cd temp_${Temp%.*}
 
-		for i in 1 2 3 4 5
+		for m in 3
 		do
-			mkdir run_$i
-			cd run_$i
+			mkdir molality_$m
+			cd molality_$m
 
-			# Coppying all needed files to run folder (alphabetical order)
-			cp ../../../input/runMD .
-			cp ../../../input/simulation.in .
-			cp ../../../input/restarts/temp_${Temp%.*}/molality_$m/1.restart .
+			for i in 1 2 3 4 5
+			do
+				mkdir run_$i
+				cd run_$i
 
-			# Set simulation_preprocessing.in file values
-			randomNumber=$(shuf -i 1-100 -n1)
-			sed -i 's/R_VALUE/'$randomNumber'/' simulation.in
-			sed -i 's/T_VALUE/'$Temp'/' simulation.in
+				# Coppying all needed files to run folder (alphabetical order)
+				cp ../../../../input/runMD .
+				cp ../../../../input/simulation.in .
+				cp ../../../../input/restarts/temp_${Temp%.*}/molality_$m/1.restart .
 
-			# Set runMD variables
-			sed -i 's/JOB_NAME/Li2SO4 T is '${Temp%.*}' m is '$m' run '$i'/' runMD
-			sed -i 's/INPUT/simulation.in/' runMD
+				# Set simulation_preprocessing.in file values
+				randomNumber=$(shuf -i 1-100 -n1)
+				sed -i 's/R_VALUE/'$randomNumber'/' simulation.in
+				sed -i 's/T_VALUE/'$Temp'/' simulation.in
+				sed -i 's/Nrun_VALUE/'$Nrun'/' simulation.in
 
-			# Commiting run and reporting that
-			sbatch runMD
-			echo "Runtask commited: T="$Temp", m="$m", run " $i"."
+				# Set runMD variables
+				sed -i 's/JOB_NAME/Li2SO4 T is '${Temp%.*}' m is '$m' run '$i'/' runMD
+				sed -i 's/INPUT/simulation.in/' runMD
+
+				# Commiting run and reporting that
+				sbatch runMD
+				echo "Runtask commited: T="$Temp", m="$m", run " $i"."
+				cd ..
+			done
 			cd ..
 		done
 		cd ..
 	done
-
 	cd ..
 done
